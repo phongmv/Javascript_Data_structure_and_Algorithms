32188147f38ebd8fca97467a836da9f89be3e94a
jindev <maiphong1796@gmail.com>
2024-10-02 15:51:23 +0700

    array: twosum

---
+++ Data_structure_Array/two_sum.js
@@ -0,0 +1,24 @@
+function twoSum(arr, target) {
+    const map = new Map()
+
+    for (let i = 0 ; i < arr.length ; i++) {
+
+        let remaining = target - arr[i]
+
+        if(!map.has(remaining)){
+            map.set(arr[i], i)
+        }
+        else  {
+            return [map.get(remaining), i]
+        }
+    }
+
+    return  []
+}
+
+
+
+// Test Cases
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // [1, 2]
+console.log(twoSum([3, 3], 6)); // [0, 1]
