1119d58b1c20a04dee9aa64283e7cb489a366aac
jindev <maiphong1796@gmail.com>
2024-10-04 17:31:28 +0700

    refactor: implement hash table

--- Data_stucture_Hash_table/implement_hash_table.js
+++ Data_stucture_Hash_table/implement_hash_table.js
@@ -5,20 +5,32 @@ class HashTable {
 
     _hash(key) {
         let hash = 0;
-        for (let i =0; i < key.length; i++){
+        for (let i = 0; i < key.length; i++){
             hash = (hash + key.charCodeAt(i) * i) % this.data.length
         }
         return hash;
     }
 
 
+    set(key, value){
+        const address = this._hash(key)
+        if(!this.data[address]) {
+            this.data[address] = []
+        }
+        this.data[address].push([key, value])
+        return this.data
+    }
 
-
+    get(key){
+        const address = this._hash(key)
+        if(!this.data[address]) return undefined
+        return this.data[address]
+    }
 
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-myHashTable.set('apples', 9)
-myHashTable.get('apples')
+console.log(myHashTable.get('grapas'))
+
+
